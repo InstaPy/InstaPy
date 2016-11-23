eb05b7b4496ecafcd8bf63dd148717a8c336536e
timgrossmann <t.grossmann1994@web.de>
2016-11-23 11:13:52 +0100

    small bug fixes

--- instapy/print_log_writer.py
+++ instapy/print_log_writer.py
@@ -1,4 +1,5 @@
 """Module only used to log the number of followers to a file"""
+from datetime import datetime
 
 def log_follower_num(browser, username):
   """Prints and logs the current number of followers to
@@ -6,7 +7,8 @@ def log_follower_num(browser, username):
   browser.get('https://www.instagram.com/' + username)
 
   follower_elem = browser.find_element_by_xpath\
-    ('//a[@href="/' + username + '/followers/"]')
+    ('//a[@href="/' + username.lower() + '/followers/"]')
 
-  with open('./followerNum.txt', 'a') as numFile:
-    numFile.write(follower_elem.text + '355')
+  with open('./logs/followerNum.txt', 'a') as numFile:
+    numFile.write(datetime.now().strftime('%Y-%m-%d %H:%M '))
+    numFile.write(follower_elem.text + '\n')
