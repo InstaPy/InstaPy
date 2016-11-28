98ca5850f1370d40ffbf880eda385d6fbfcf5774
timgrossmann <t.grossmann1994@web.de>
2016-11-28 09:39:26 +0100

    changed few things

--- tester.py
+++ tester.py
@@ -1,4 +1,7 @@
+from random import randint
+from time import sleep
 from instapy import InstaPy
+
 tag_list = ['sex','nude','naked','beef','pork','seafood',
             'egg','chicken','cheese','sausage','lobster',
             'fisch','schwein','lamm','rind','kuh','meeresfrüchte',
@@ -28,16 +31,33 @@ natureComments = ['Epic!', 'Perfect', 'Would love to visit this Place!',
                   'Beautiful', 'Impressive!', 'Awesome!', 'Incredible!',
                   ]
 
-InstaPy()\
-  .login()\
-  .set_dont_like(tag_list)\
-  .set_dont_include(friendList)\
-  .set_ignore_if_contains(ignore_list)\
-  .set_do_comment(enabled=True, percentage=10)\
-  .set_comments(comments=normalComments)\
-  .set_use_clarifai(enabled=True)\
-  .clarifai_check_img_for(['nsfw'])\
-  .clarifai_check_img_for(['food', 'lunch', 'dinner'], comment=True, comments=foodComments)\
-  .clarifai_check_img_for(['lake', 'landscape', 'mountain'], comment=True, comments=natureComments)\
-  .like_by_tags(['vegan', 'veganfoodshare', 'veganfood', 'nature'], amount=100)\
-  .end()
+def startScript():
+  InstaPy()\
+    .login()\
+    .set_dont_like(tag_list)\
+    .set_dont_include(friendList)\
+    .set_ignore_if_contains(ignore_list)\
+    .set_do_comment(enabled=True, percentage=10)\
+    .set_comments(comments=normalComments)\
+    .set_use_clarifai(enabled=True)\
+    .clarifai_check_img_for(['nsfw'])\
+    .clarifai_check_img_for(['food', 'lunch', 'dinner'], comment=True, comments=foodComments)\
+    .clarifai_check_img_for(['lake', 'landscape', 'mountain'], comment=True, comments=natureComments)\
+    .like_by_tags(['vegan', 'veganfoodshare', 'veganfood', 'whatveganseat','nature'], amount=100)\
+    .end()
+
+runFile = open('/root/InstaPy/logs/run.txt', 'a')
+
+
+if randint(0,10) < 6:
+  try:
+    runFile.write('started\n')
+    runFile.close()
+    sleep(randint(0, 60 * 30))
+    startScript()
+  except Exception as err:
+    with open('/root/erros.log', 'a') as errLog:
+      errLog.write(str(err) + '\n')
+else:
+  runFile.write('not started\n')
+  runFile.close()
