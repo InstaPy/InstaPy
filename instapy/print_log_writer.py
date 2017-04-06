d880074df73e276280741b0f7f491d52194f227f
timgrossmann <t.grossmann1994@web.de>
2017-04-06 23:10:08 +0200

    fixed problem with no follower count on completely new account

--- instapy/print_log_writer.py
+++ instapy/print_log_writer.py
@@ -1,14 +1,19 @@
 """Module only used to log the number of followers to a file"""
 from datetime import datetime
+from selenium.common.exceptions import NoSuchElementException
 
 def log_follower_num(browser, username):
   """Prints and logs the current number of followers to
   a seperate file"""
   browser.get('https://www.instagram.com/' + username)
+  follower_num = 0
 
-  follower_elem = browser.find_element_by_xpath\
-    ('//a[@href="/' + username.lower()  + '/followers/"]')
+  try:
+    follower_num = browser.find_element_by_xpath\
+      ('//a[@href="/' + username.lower()  + '/followers/"]').text
+  except NoSuchElementException:
+    follower_num = 0
 
   with open('./logs/followerNum.txt', 'a') as numFile:
     numFile.write(datetime.now().strftime('%Y-%m-%d %H:%M '))
-    numFile.write(follower_elem.text + '\n')
+    numFile.write(str(follower_num) + '\n')
