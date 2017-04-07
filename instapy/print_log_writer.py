fb4641734497d6c81f306e894d87603e8692c88f
timgrossmann <t.grossmann1994@web.de>
2017-04-07 17:28:04 +0200

    changed to use execute script

--- instapy/print_log_writer.py
+++ instapy/print_log_writer.py
@@ -6,13 +6,12 @@ def log_follower_num(browser, username):
   """Prints and logs the current number of followers to
   a seperate file"""
   browser.get('https://www.instagram.com/' + username)
+
   follower_num = 0
 
-  try:
-    follower_num = browser.find_element_by_xpath\
-      ('//a[@href="/' + username.lower()  + '/followers/"]').text
-  except NoSuchElementException:
-    follower_num = 0
+  followed_by = browser.execute_script("return window._sharedData.entry_data.ProfilePage[0].user.followed_by.count")
+  if followed_by is not None:
+    follower_num = followed_by
 
   with open('./logs/followerNum.txt', 'a') as numFile:
     numFile.write(datetime.now().strftime('%Y-%m-%d %H:%M '))
