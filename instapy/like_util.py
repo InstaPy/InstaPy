9fd1c70d88e3712542e0ead3f1473ead8da41f6c
Pedro Paiva <cpaiva.pedro@gmail.com>
2017-04-10 20:16:37 -0300

    Minor fix (forgot about one return!)

--- instapy/like_util.py
+++ instapy/like_util.py
@@ -77,7 +77,7 @@ def check_link(browser, link, dont_like, ignore_if_contains, username):
   post_page = browser.execute_script("return window._sharedData.entry_data.PostPage")
   if post_page is None:
     print('Unavailable Page: ' + link)
-    return False, 'Unavailable Page'
+    return False, 'Unavailable Page', None
 
   """Gets the description of the link and checks for the dont_like tags"""
   is_video = browser.execute_script("return window._sharedData.entry_data.PostPage[0].media.is_video")
