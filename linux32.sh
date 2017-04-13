0eb6c3e3b8080c8032ca2cdd70099bc10541f25a
BinHong Lee <binhong@binhong.me>
2017-04-12 22:16:42 -0700

    Added Linux Mint Install Script

---
+++ linux32.sh
@@ -0,0 +1,20 @@
+echo "InstaPy Linux Setup"
+echo =============================================================================================
+echo "Downloading Chrome Driver..."
+wget https://chromedriver.storage.googleapis.com/2.29/chromedriver_linux32.zip
+echo "Chrome Driver download completed."
+echo " "
+echo "Unzipping Chrome Driver..."
+unzip chromedriver_linux32.zip
+mv ./chromedriver ./assets/chromedriver
+echo "Unzipping completed."
+echo " "
+echo "Removing unneeded files..."
+rm chromedriver_linux32.zip
+echo "Removal completed."
+echo " "
+sudo pip install setuptools
+sudo pip install python-dev
+sudo pip install ./
+echo "Setup is completed."
+read -n1 -r -p "Press any key to continue..." key
