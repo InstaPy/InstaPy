dc8260869f709759a66b557f3d4c8a2694d6b149
Eduardo Calazans Júnior <eduardoc_jr@me.com>
2017-04-18 14:31:15 -0300

    Fixed login_util
    Changed the elements and make it work on every login scenario.
    
    Also added the support to ActionChains to avoid Instagram anti-bot
    protection and simulate mouse movements

--- instapy/login_util.py
+++ instapy/login_util.py
@@ -1,31 +1,30 @@
 """Module only used for the login part of the script"""
-from selenium.common.exceptions import NoSuchElementException
-
 from .time_util import sleep
-
+from selenium.webdriver.common.action_chains import ActionChains
 
 def login_user(browser, username, password):
   """Logins the user with the given username and password"""
   browser.get('https://www.instagram.com')
 
-  #clicks the log in if you have an account link
-  login_elem = browser.find_element_by_class_name('_fcn8k')
-  login_elem.click()
+  #Check if the first div is 'Create an Account' or 'Log In'
+  login_elem = browser.find_element_by_xpath("//article/div/div/p/a[text()='Log in']")
+  if login_elem is not None:
+    action = ActionChains(browser).move_to_element(login_elem).click().perform()
 
-  # enters username and password and logs the user in
-  username_elem = browser.find_element_by_name('username')
-  passwd_elem = browser.find_element_by_name('password')
-  login_div = browser.find_element_by_class_name('_uikn3')
-  login_elem = login_div.find_element_by_tag_name('button')
+  #Enter username and password and logs the user in
+  #Sometimes the element name isn't 'Username' and 'Password' (valid for placeholder too)
+  inputs = browser.find_elements_by_xpath("//form/div/input")
+  action = ActionChains(browser).move_to_element(inputs[0]).click().send_keys(username) \
+          .move_to_element(inputs[1]).click().send_keys(password).perform()
 
-  username_elem.send_keys(username)
-  passwd_elem.send_keys(password)
-  login_elem.click()
+  login_button = browser.find_element_by_xpath("//form/span/button[text()='Log in']")
+  action = ActionChains(browser).move_to_element(login_button).click().perform()
 
   sleep(2)
-
-  try:
-    browser.find_element_by_class_name('_q90d5')
-    return False
-  except NoSuchElementException as err:
+  
+  #Check if user is logged-in (If there's two 'nav' elements)
+  nav = browser.find_elements_by_xpath('//nav')
+  if len(nav) == 2:
     return True
+  else:
+    return False
