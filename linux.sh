d58d349c1935b35a6e952bd406a2251a3d28cea1
BinHong Lee <binhong@binhong.me>
2017-04-22 15:11:50 -0700

    Added linux.sh support for Mac as requested

--- linux.sh
+++ linux.sh
@@ -1,25 +1,34 @@
-echo "InstaPy Linux Setup"
+echo "InstaPy Setup"
 echo =============================================================================================
 echo "Downloading Chrome Driver..."
 arch=$(getconf LONG_BIT)
-if [ $arch == "64" ]; then
-  wget https://chromedriver.storage.googleapis.com/2.29/chromedriver_linux64.zip -O chromedriver_linux.zip
+kernel=$(uname)
+if [ $kernel == "Darwin" ]; then
+  curl -o chromedriver.zip -O https://chromedriver.storage.googleapis.com/2.29/chromedriver_mac64.zip
 else
-  wget https://chromedriver.storage.googleapis.com/2.29/chromedriver_linux32.zip -O chromedriver_linux.zip
+  if [ $arch == "64" ]; then
+    wget https://chromedriver.storage.googleapis.com/2.29/chromedriver_linux64.zip -O chromedriver.zip
+  else
+    wget https://chromedriver.storage.googleapis.com/2.29/chromedriver_linux32.zip -O chromedriver.zip
+  fi
 fi
 echo "Chrome Driver download completed."
 echo " "
 echo "Unzipping Chrome Driver..."
-unzip chromedriver_linux.zip
+unzip chromedriver.zip
 mv ./chromedriver ./assets/chromedriver
 echo "Unzipping completed."
 echo " "
 echo "Removing unneeded files..."
-rm chromedriver_linux.zip
+rm chromedriver.zip
 echo "Removal completed."
 echo " "
-sudo pip install setuptools
-sudo apt-get install python-dev
-sudo pip install ./
+if [ $kernel == "Darwin" ]; then
+  sudo python setup.py install
+else
+  sudo pip install setuptools
+  sudo apt-get install python-dev
+  sudo pip install ./
+fi
 echo "Setup is completed."
 read -n1 -r -p "Press any key to continue..." key
