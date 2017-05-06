dff4312e2fbf10499108666202b6d129b73bbc45
Andrew Byrd <byrd286@gmail.com>
2017-05-06 11:47:08 -0400

    Fix issue with find elements returning incorrect following user list

--- instapy/unfollow_util.py
+++ instapy/unfollow_util.py
@@ -17,9 +17,8 @@ def unfollow(browser, username, amount, dont_include):
   sleep(2)
 
   person_list_div = browser.find_element_by_class_name('_4gt3b')
-  person_list = person_list_div.find_elements_by_class_name('_cx1ua')
-
-  person_list = [x.find_element_by_xpath('//div[@class="_gzjax"]/a').text for x in person_list]
+  person_list = person_list_div.find_elements_by_xpath("//a[contains(concat(' ', normalize-space(@class), ' '), ' _4zhc5 ')]")
+  person_list = [x.text for x in person_list]
 
   follow_div = browser.find_element_by_class_name('_4gt3b')
   follow_buttons = follow_div.find_elements_by_tag_name('button')
