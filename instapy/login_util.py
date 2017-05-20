7d5ca95b407b2b39ba1b8ea16026c6755f378184
TGLIDE <thomglopes@gmail.com>
2017-05-19 23:54:54 -0300

    Update login_util.py to avoid errors
    When the instagram website is in, for example, Brazillian Portuguese, there are errors because of the change of the name of certain html elements. So, this simple change fixes it.

--- instapy/login_util.py
+++ instapy/login_util.py
@@ -6,6 +6,11 @@ def login_user(browser, username, password):
   """Logins the user with the given username and password"""
   browser.get('https://www.instagram.com')
 
+  # Changes instagram language to english, to ensure no errors ensue from having the site on a different language
+  browser.find_element_by_xpath("//footer[@class='_oofbn']/div[@class='_mhrsk _pcuq6']/nav["
+                                "@class='_p1gbi']/ul[@class='_fh0f2']/li[@class='_fw3ds'][10]/"
+                                "span[@class='_17z9g']/select[@class='_nif11']/option"
+                                "[text()='English']").click()
   #Check if the first div is 'Create an Account' or 'Log In'
   login_elem = browser.find_element_by_xpath("//article/div/div/p/a[text()='Log in']")
   if login_elem is not None:
