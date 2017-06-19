2be6873cb2fbc2dd36fa9f9c8f4e9666069d501f
matthieu jungfer <matjungfer@gmail.com>
2017-06-19 19:26:19 +0200

    unfollow with pool feature

--- instapy/print_log_writer.py
+++ instapy/print_log_writer.py
@@ -10,3 +10,20 @@ def log_follower_num(browser, username):
   followed_by = browser.execute_script("return window._sharedData.entry_data.ProfilePage[0].user.followed_by.count")
   with open('./logs/followerNum.txt', 'a') as numFile:
     numFile.write('{:%Y-%m-%d %H:%M} {}\n'.format(datetime.now(), followed_by or 0))
+
+
+def log_followed_pool(login, followed):
+  """Prints and logs the followed to
+  a seperate file"""
+  with open('./logs/' + login + '_followedPool.csv', 'a') as followPool:
+    followPool.write('"' + followed + '"' + ",")
+
+def delete_line_from_file(filepath, lineToDelete):
+    f = open(filepath,"r")
+    lines = f.readlines()
+    f.close()
+    f = open(filepath,"w")
+    for line in lines:
+      if line!= lineToDelete:
+        f.write(line)
+    f.close()
