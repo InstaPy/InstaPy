318b4d9b04c2df91b0bcfe0a74d64315945ecccd
matthieu jungfer <matjungfer@gmail.com>
2017-06-19 19:26:19 +0200

    unfollow with pool feature

--- instapy/like_util.py
+++ instapy/like_util.py
@@ -125,7 +125,7 @@ def get_links_for_tag(browser, tag, amount, media=None):
   link_elems = main_elem.find_elements_by_tag_name('a')
   total_links = len(link_elems)
   links = [link_elem.get_attribute('href') for link_elem in link_elems
-           if link_elem.text in media]
+           if link_elem and link_elem.text in media]
   filtered_links = len(links)
 
   while (filtered_links < amount) and not abort:
