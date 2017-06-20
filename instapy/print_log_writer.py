aa9a14db0398c7845735d10ec5130d90fd8c2a1b
matthieu jungfer <matjungfer@gmail.com>
2017-06-20 10:17:51 +0200

    feature fix

--- instapy/print_log_writer.py
+++ instapy/print_log_writer.py
@@ -8,6 +8,7 @@ def log_follower_num(browser, username):
   browser.get('https://www.instagram.com/' + username)
 
   followed_by = browser.execute_script("return window._sharedData.entry_data.ProfilePage[0].user.followed_by.count")
+
   with open('./logs/followerNum.txt', 'a') as numFile:
     numFile.write('{:%Y-%m-%d %H:%M} {}\n'.format(datetime.now(), followed_by or 0))
 
@@ -15,15 +16,19 @@ def log_follower_num(browser, username):
 def log_followed_pool(login, followed):
   """Prints and logs the followed to
   a seperate file"""
+
   with open('./logs/' + login + '_followedPool.csv', 'a') as followPool:
-    followPool.write('"' + followed + '"' + ",")
+    followPool.write(followed + ",\n")
+    followPool.close()
 
 def delete_line_from_file(filepath, lineToDelete):
     f = open(filepath,"r")
     lines = f.readlines()
     f.close()
     f = open(filepath,"w")
+
     for line in lines:
+
       if line!= lineToDelete:
         f.write(line)
     f.close()
