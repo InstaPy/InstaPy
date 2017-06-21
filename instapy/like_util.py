5e0532ff19d4822a9a1904842716887906b4479c
matthieu jungfer <matjungfer@gmail.com>
2017-06-21 14:02:27 +0200

    fixing bug for like_util.py l128 Message: stale element reference: element is not attached to the page document

--- instapy/like_util.py
+++ instapy/like_util.py
@@ -124,9 +124,15 @@ def get_links_for_tag(browser, tag, amount, media=None):
   main_elem = browser.find_element_by_tag_name('main')
   link_elems = main_elem.find_elements_by_tag_name('a')
   total_links = len(link_elems)
-  links = [link_elem.get_attribute('href') for link_elem in link_elems
+  links = []
+  try:
+      if link_elems:
+        links = [link_elem.get_attribute('href') for link_elem in link_elems
            if link_elem and link_elem.text in media]
-  filtered_links = len(links)
+
+      filtered_links = len(links)
+  except BaseException as e:
+      print("link_elems error \n", str(e))
 
   while (filtered_links < amount) and not abort:
     amount_left = amount - filtered_links
