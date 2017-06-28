93d2a83357e7c979047385df9f470b7fbb6fa943
Saurabh Kumar <saurabh.kumar2014bce1033@vit.ac.in>
2017-06-28 12:30:17 +0530

    #324 Added - like feed feature

---
+++ instapy/feed_util.py
@@ -0,0 +1,53 @@
+import re
+
+"""Module that handles the like features"""
+from math import ceil
+from re import findall
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+
+from .time_util import sleep
+import random
+
+LIKE_TAG_CLASS = 'coreSpriteHeartOpen'
+
+def get_like_on_feed(
+	browser,
+	amount,
+	):
+	'''
+		browser - the selenium browser element
+		amount - total amount of likes to perform
+
+		--------------------------------------
+		The function takes in the total amount of likes to perform 
+		and what is the probability to cast the like on an image
+	'''
+
+	global SLEEP_AMOUNT
+
+	# get like buttons
+	likes_performed = 0
+
+	while likes_performed <= amount:
+		# get the like buttons
+		like_buttons = []
+		try:
+			like_buttons = browser.find_elements_by_class_name(LIKE_TAG_CLASS)
+		except selenium.common.exceptions.NoSuchElementException:
+			print ('Unale to find the like buttons')
+			exit(0)
+
+		for button in like_buttons:
+			likes_performed += 1
+			if not (likes_performed <= amount):
+				print ('Performed the required number of likes')
+				break
+			yield button
+
+		print ('---> Total Likes uptil now ->', likes_performed)
+		browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+
+
+	
