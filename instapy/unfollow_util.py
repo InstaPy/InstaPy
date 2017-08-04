6316c8b5491a2d8a93121df5f2a59c97719f854c
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2017-08-04 20:12:36 -0300

    Keep track of followed users

--- instapy/unfollow_util.py
+++ instapy/unfollow_util.py
@@ -120,20 +120,61 @@ def unfollow(browser, username, amount, dont_include, onlyInstapyFollowed, autom
 
     return unfollowNum
 
-def follow_given_user_followers(browser, user_name, amount, dont_include):
+
+
+def follow_user(browser, follow_restrict, login, user_name):
+    """Follows the user of the currently opened image"""
+
+    follow_button = browser.find_element_by_xpath("//article/header/span/button")
+    sleep(2)
+
+    if follow_button.text == 'Follow':
+        follow_button.click()
+        print('--> Now following')
+        log_followed_pool(login, user_name)
+        follow_restrict[user_name] = follow_restrict.get(user_name, 0) + 1
+        sleep(3)
+        return 1
+
+    else:
+        print('--> Already following')
+        sleep(1)
+        return 0
+
+
+def follow_given_user(browser, acc_to_follow, follow_restrict):
+    """Follows a given user."""
+    browser.get('https://www.instagram.com/' + acc_to_follow)
+    print('--> {} instagram account is opened...'.format(acc_to_follow))
+    follow_button = browser.find_element_by_xpath("//*[contains(text(), 'Follow')]")
+    sleep(10)
+    if follow_button.text == 'Follow':
+        follow_button.click()
+        print('---> Now following: {}'.format(acc_to_follow))
+        print('*' * 20)
+        follow_restrict[acc_to_follow] = follow_restrict.get(acc_to_follow, 0) + 1
+        sleep(3)
+        return 1
+    else:
+        print('---> {} is already followed'.format(acc_to_follow))
+        print('*' * 20)
+        sleep(3)
+        return 0
+
+def follow_given_user_followers(browser, user_name, amount, dont_include, login, follow_restrict):
     browser.get('https://www.instagram.com/' + user_name)
 
     followNum = 0
 
     #  check how many poeple are following this user.
-    allfollowing = formatNumber(browser.find_element_by_xpath("//li[3]/a/span").text)
+    allfollowing = formatNumber(browser.find_element_by_xpath("//li[2]/a/span").text)
 
     #  throw RuntimeWarning if we are 0 people following this user
     if (allfollowing == 0):
         raise RuntimeWarning('There are 0 people to follow')
 
     try:
-        following_link = browser.find_elements_by_xpath('//header/div[2]//li[3]')
+        following_link = browser.find_elements_by_xpath('//header/div[2]//li[2]')
         following_link[0].click()
     except BaseException as e:
         print("following_link error \n", str(e))
@@ -177,6 +218,8 @@ def follow_given_user_followers(browser, user_name, amount, dont_include):
 
                 followNum += 1
                 button.click()
+                log_followed_pool(login, user_name)
+                follow_restrict[user_name] = follow_restrict.get(user_name, 0) + 1
 
                 print('--> Ongoing follow ' + str(followNum) + ', now following: {}'.format(
                     person.encode('utf-8')))
@@ -193,47 +236,6 @@ def follow_given_user_followers(browser, user_name, amount, dont_include):
         print("follow loop error \n", str(e))
 
     return followNum
-
-
-
-def follow_user(browser, follow_restrict, login, user_name):
-    """Follows the user of the currently opened image"""
-
-    follow_button = browser.find_element_by_xpath("//article/header/span/button")
-    sleep(2)
-
-    if follow_button.text == 'Follow':
-        follow_button.click()
-        print('--> Now following')
-        log_followed_pool(login, user_name)
-        follow_restrict[user_name] = follow_restrict.get(user_name, 0) + 1
-        sleep(3)
-        return 1
-
-    else:
-        print('--> Already following')
-        sleep(1)
-        return 0
-
-
-def follow_given_user(browser, acc_to_follow, follow_restrict):
-    """Follows a given user."""
-    browser.get('https://www.instagram.com/' + acc_to_follow)
-    print('--> {} instagram account is opened...'.format(acc_to_follow))
-    follow_button = browser.find_element_by_xpath("//*[contains(text(), 'Follow')]")
-    sleep(10)
-    if follow_button.text == 'Follow':
-        follow_button.click()
-        print('---> Now following: {}'.format(acc_to_follow))
-        print('*' * 20)
-        follow_restrict[acc_to_follow] = follow_restrict.get(acc_to_follow, 0) + 1
-        sleep(3)
-        return 1
-    else:
-        print('---> {} is already followed'.format(acc_to_follow))
-        print('*' * 20)
-        sleep(3)
-        return 0
 
 
 def dump_follow_restriction(followRes):
