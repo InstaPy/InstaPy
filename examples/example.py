b886e191d0bc68ee7352c288688c1ed0bf68f90c
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2017-08-05 00:21:58 -0300

    example for following followers

--- examples/example.py
+++ examples/example.py
@@ -66,6 +66,11 @@ session.like_from_image(url='www.instagram.com/image', amount=100)
 # media filtering works here as well
 session.like_by_tags(['#test'], amount=10, media='Video')
 
+# follows the followers of a given user
+session.follow_user_followers('friend1', amount=10)
+# follows the people that a given user is following
+session.follow_user_following('friend2', amount=10)
+
 session.unfollow_users(
     amount=10)  # unfollows 10 of the accounts your following -> instagram will only unfollow 10 before you'll be 'blocked
 #  for 10 minutes' (if you enter a higher number than 10 it will unfollow 10, then wait 10 minutes and will continue then)
