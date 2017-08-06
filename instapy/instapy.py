83c0ce4a8f3b3eca17355badb09263c59e596b00
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2017-08-06 18:14:49 -0300

    Function to like users posts

--- instapy/instapy.py
+++ instapy/instapy.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from os import environ
 
 from random import randint
+from random import sample
 from pyvirtualdisplay import Display
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
@@ -29,10 +30,137 @@ from .unfollow_util import load_follow_restriction
 from .unfollow_util import dump_follow_restriction
 from .unfollow_util import set_automated_followed_pool
 from .feed_util import get_like_on_feed
+from .like_util import get_posts_by_user
 
 
 class InstaPy:
     """Class to be instantiated to use the script"""
+
+    def like_by_user(self, user_names, amount=10, is_random=False, media=None):
+        if self.aborting:
+            return self
+        liked_img = 0
+        already_liked = 0
+        inap_img = 0
+        commented = 0
+        followed = 0
+
+        user_names = user_names or []
+
+        for index, user_name in enumerate(user_names):
+            print('User_name [{}/{}]'.format(index + 1, len(user_names)))
+            print('--> {}'.format(user_name.encode('utf-8')))
+            self.logFile.write('User_name [{}/[]]'.format(index + 1, len(user_names)))
+            self.logFile.write('--> {}\n'.format(user_name.encode('utf-8')))
+
+            try:
+                raw_links = get_posts_by_user(self.browser, user_name, amount, is_random)
+            except NoSuchElementException:
+                print('Too few images, aborting')
+                self.logFile.write('Too few images, aborting\n')
+
+                self.aborting = True
+                return self
+
+            picked_lnks = []
+            if is_random:
+                samples = sample(range(0, len(raw_links)), amount)
+
+                for x in samples:
+                    picked_lnks.append(raw_links[x])
+            else:
+                picked_lnks = raw_links
+
+
+            for i, link in enumerate(picked_lnks):
+                print('[{}/{}]'.format(i + 1, len(picked_lnks)))
+                self.logFile.write('[{}/{}]'.format(i + 1, len(picked_lnks)))
+                self.logFile.write(link)
+
+                try:
+                    inappropriate, user_name, is_video, reason = \
+                        check_link(self.browser, link, self.dont_like, self.ignore_if_contains, self.ignore_users,
+                                   self.username, self.like_by_followers_upper_limit,
+                                   self.like_by_followers_lower_limit)
+
+                    if not inappropriate:
+                        liked = like_image(self.browser)
+
+                        if liked:
+                            liked_img += 1
+                            checked_img = True
+                            temp_comments = []
+                            commenting = randint(0, 100) <= self.comment_percentage
+                            following = randint(0, 100) <= self.follow_percentage
+
+                            if self.use_clarifai and (following or commenting):
+                                try:
+                                    checked_img, temp_comments = \
+                                        check_image(self.browser, self.clarifai_id,
+                                                    self.clarifai_secret,
+                                                    self.clarifai_img_tags,
+                                                    self.clarifai_full_match)
+                                except Exception as err:
+                                    print('Image check error: {}'.format(err))
+                                    self.logFile.write('Image check error: {}\n'.format(err))
+
+                            if self.do_comment and user_name not in self.dont_include \
+                                    and checked_img and commenting:
+                                if temp_comments:
+                                    # Use clarifai related comments only!
+                                    comments = temp_comments
+                                elif is_video:
+                                    comments = self.comments + self.video_comments
+                                else:
+                                    comments = self.comments + self.photo_comments
+                                commented += comment_image(self.browser, comments)
+                            else:
+                                print('--> Not commented')
+                                sleep(1)
+
+                            if self.do_follow and user_name not in self.dont_include \
+                                    and checked_img and following \
+                                    and self.follow_restrict.get(user_name, 0) < self.follow_times:
+                                followed += follow_user(self.browser, self.follow_restrict, self.username, user_name)
+                            else:
+                                print('--> Not following')
+                                sleep(1)
+                        else:
+                            already_liked += 1
+                            if is_random:
+
+                                repickedNum = -1
+                                while repickedNum == -1 or raw_links[repickedNum] not in picked_lnks:
+                                    repickedNum = randint(0, len(raw_links)-1)
+                                    print("PICKED - > " + str(repickedNum) + " -__- " + str(len(raw_links)))
+                                picked_lnks.append(raw_links[repickedNum])
+                                print("--------------> REPICKING -> " + str(raw_links[repickedNum]))
+                    else:
+                        print('--> Image not liked: {}'.format(reason))
+                        inap_img += 1
+                except NoSuchElementException as err:
+                    print('Invalid Page: {}'.format(err))
+                    self.logFile.write('Invalid Page: {}\n'.format(err))
+
+                print('')
+                self.logFile.write('\n')
+
+        print('Liked: {}'.format(liked_img))
+        print('Already Liked: {}'.format(already_liked))
+        print('Inappropriate: {}'.format(inap_img))
+        print('Commented: {}'.format(commented))
+        print('Followed: {}'.format(followed))
+
+        self.logFile.write('Liked: {}\n'.format(liked_img))
+        self.logFile.write('Already Liked: {}\n'.format(already_liked))
+        self.logFile.write('Inappropriate: {}\n'.format(inap_img))
+        self.logFile.write('Commented: {}\n'.format(commented))
+        self.logFile.write('Followed: {}\n'.format(followed))
+
+        self.followed += followed
+
+        return self
+
 
     def __init__(self, username=None, password=None, nogui=False, selenium_local_session=True, use_firefox=False, page_delay=25):
         if nogui:
