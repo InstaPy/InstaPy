d7194c3a11a32d37bf2b9bfdbbc21935343ae4f9
benvegel <benvegel@gmail.com>
2017-08-08 04:37:44 -0400

    changes to pull request to close #502

--- instapy/login_util.py
+++ instapy/login_util.py
@@ -9,11 +9,11 @@ def login_user(browser, username, password, switch_language = True):
 
     # Changes instagram language to english, to ensure no errors ensue from having the site on a different language
     # Might cause problems if the OS language is english
-    #if switch_language:
-    #    browser.find_element_by_xpath("//footer[@class='_s5vm9']/div[@class='_g7lf5 _9z659']/nav["
-    #                                  "@class='_luodr']/ul[@class='_g8wl6']/li[@class='_538w0'][10]/"
-    #                                  "span[@class='_pqycz _hqmnd']/select[@class='_fsoey']/option"
-    #                                  "[text()='English']").click()
+    if switch_language:
+        browser.find_element_by_xpath("//footer[@class='_s5vm9']/div[@class='_g7lf5 _9z659']/nav["
+                                      "@class='_luodr']/ul[@class='_g8wl6']/li[@class='_538w0'][10]/"
+                                      "span[@class='_pqycz _hqmnd']/select[@class='_fsoey']/option"
+                                      "[text()='English']").click()
 
     # Check if the first div is 'Create an Account' or 'Log In'
     login_elem = browser.find_element_by_xpath("//article/div/div/p/a[text()='Log in']")
