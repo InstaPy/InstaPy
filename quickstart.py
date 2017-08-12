9028ba473a3cb788f14a5f83120541645759ccc4
Abdul Abhilasha <30963485+AbdulABHILASHA@users.noreply.github.com>
2017-08-12 19:02:17 -0300

    Create quickstart.py

---
+++ quickstart.py
@@ -0,0 +1,16 @@
+from instapy import InstaPy
+
+insta_username = ''
+insta_password = ''
+
+InstaPy(username=insta_username, password=insta_password, use_firefox=False, page_delay=25)\
+  .login()\
+  .set_switch_language(False)\
+  .set_upper_follower_count(limit=2500)\
+  .set_do_comment(True, percentage=10)\
+  .set_comments(['aMEIzing!', 'So much fun!!', 'Nicey!'])\
+  .set_dont_include(['friend1', 'friend2', 'friend3'])\
+  .set_dont_like(['pizza', 'girl'])\
+  .set_ignore_if_contains(['cow'])\
+  .like_by_tags(['kanisha', 'chickenbutt'], amount=100)\
+  .end()
