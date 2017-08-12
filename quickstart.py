a7df3ee49bfb33c993175ab2e11b1ea4b10acb0b
Abdul Abhilasha <30963485+AbdulABHILASHA@users.noreply.github.com>
2017-08-12 19:12:20 -0300

    Fixing my mistakes in  set_switch_language(False)\

--- quickstart.py
+++ quickstart.py
@@ -5,7 +5,6 @@ insta_password = ''
 
 InstaPy(username=insta_username, password=insta_password, use_firefox=False, page_delay=25)\
   .login()\
-  .set_switch_language(False)\
   .set_upper_follower_count(limit=2500)\
   .set_do_comment(True, percentage=10)\
   .set_comments(['aMEIzing!', 'So much fun!!', 'Nicey!'])\
