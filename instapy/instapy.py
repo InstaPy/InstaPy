5cabb6ddb76005db412cbcc53aa84cb55f0f48dc
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2017-08-12 00:29:16 -0300

    Interact_with_following

--- instapy/instapy.py
+++ instapy/instapy.py
@@ -25,6 +25,7 @@ from .print_log_writer import log_follower_num
 from .time_util import sleep
 from .util import formatNumber
 from .unfollow_util import get_given_user_followers
+from .unfollow_util import get_given_user_following
 from .unfollow_util import unfollow
 from .unfollow_util import follow_given_user_followers
 from .unfollow_util import follow_given_user_following
@@ -844,6 +845,34 @@ class InstaPy:
         try:
             for user in usernames:
                 userToInteract += get_given_user_followers(self.browser, user, amount, self.dont_include, self.username, self.follow_restrict, random)
+        except (TypeError, RuntimeWarning) as err:
+            if type(err) == RuntimeWarning:
+                print(u'Warning: {} , stopping follow_users'.format(err))
+                self.logFile.write('Warning: {} , stopping follow_users\n'.format(err))
+
+                return self
+            else:
+                print('Sorry, an error occured: {}'.format(err))
+                self.logFile.write('Sorry, an error occured: {}\n'.format(err))
+                self.aborting = True
+
+                return self
+
+        print('--> Users: {}'.format(len(userToInteract)))
+        print('')
+        userToInteract = sample(userToInteract, ceil(self.user_interact_percentage*len(userToInteract)/100))
+        self.like_by_users(userToInteract, self.user_interact_amount, self.user_interact_random, self.user_interact_media)
+
+        return self
+
+    def interact_user_following(self, usernames, amount=10, random=False):
+
+        userToInteract = []
+        if not isinstance(usernames, list):
+            usernames = [usernames]
+        try:
+            for user in usernames:
+                userToInteract += get_given_user_following(self.browser, user, amount, self.dont_include, self.username, self.follow_restrict, random)
         except (TypeError, RuntimeWarning) as err:
             if type(err) == RuntimeWarning:
                 print(u'Warning: {} , stopping follow_users'.format(err))
--- instapy/unfollow_util.py
+++ instapy/unfollow_util.py
@@ -296,6 +296,56 @@ def get_given_user_followers(browser, user_name, amount, dont_include, login, fo
 
     return person_list
 
+
+def get_given_user_following(browser, user_name, amount, dont_include, login, follow_restrict, is_random):
+    browser.get('https://www.instagram.com/' + user_name)
+
+    #  check how many poeple are following this user.
+    allfollowing = formatNumber(browser.find_element_by_xpath("//li[3]/a/span").text)
+
+    #  throw RuntimeWarning if we are 0 people following this user
+    if (allfollowing == 0):
+        raise RuntimeWarning('There are 0 people to follow')
+
+    try:
+        following_link = browser.find_elements_by_xpath('//header/div[2]//li[3]')
+        following_link[0].click()
+    except BaseException as e:
+        print("following_link error \n", str(e))
+
+    followNum = 0
+    sleep(2)
+    person_followed = []
+
+    # find dialog box
+    dialog = browser.find_element_by_xpath('/html/body/div[4]/div/div[2]/div/div[2]/div/div[2]')
+
+    # scroll down the page
+    scroll_bottom(browser, dialog, allfollowing)
+
+    #Get follow buttons. This approch will find the follow buttons and ignore the Unfollow/Requested buttons.
+    follow_buttons = dialog.find_elements_by_xpath("//div/div/span/button[text()='Follow']")
+    person_list = []
+
+    if amount >= len(follow_buttons):
+        amount = len(follow_buttons)
+        print(user_name+" -> Less users to follow than requested.")
+
+    finalBtnPerson = []
+    if is_random:
+        sample = random.sample(range(0, len(follow_buttons)), amount)
+
+        for num in sample:
+            finalBtnPerson.append(follow_buttons[num])
+    else:
+        finalBtnPerson = follow_buttons[0:amount]
+    for person in finalBtnPerson:
+
+        if person and hasattr(person, 'text') and person.text:
+            person_list.append(person.find_element_by_xpath("../../../*").find_elements_by_tag_name("a")[1].text)
+
+    return person_list
+
 def follow_given_user_followers(browser, user_name, amount, dont_include, login, follow_restrict, random):
     browser.get('https://www.instagram.com/' + user_name)
 
