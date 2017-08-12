727d2924f8912910798f94e0d4c684b5ac347546
Tim Großmann <t.grossmann1994@web.de>
2017-08-13 00:16:09 +0200

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -3,6 +3,9 @@ from instapy import InstaPy
 insta_username = ''
 insta_password = ''
 
+# if you want to run this script on a server, 
+# simply add nogui=True to the InstaPy() constructor
+
 InstaPy(username=insta_username, password=insta_password)\
   .login()\
   .set_upper_follower_count(limit=2500)\
@@ -10,6 +13,5 @@ InstaPy(username=insta_username, password=insta_password)\
   .set_comments(['aMEIzing!', 'So much fun!!', 'Nicey!'])\
   .set_dont_include(['friend1', 'friend2', 'friend3'])\
   .set_dont_like(['pizza', 'girl'])\
-  .set_ignore_if_contains(['cow'])\
-  .like_by_tags(['kanisha', 'chickenbutt'], amount=100)\
+  .like_by_tags(['natgeo', 'world'], amount=100)\
   .end()
