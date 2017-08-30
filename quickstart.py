b1e53d86dd3b154cbaaac07f6b639606e419917a
Tim Großmann <t.grossmann1994@web.de>
2017-08-30 11:50:50 +0200

    changed quickstart from chaining to session.xxx

--- quickstart.py
+++ quickstart.py
@@ -5,13 +5,18 @@ insta_password = ''
 
 # if you want to run this script on a server, 
 # simply add nogui=True to the InstaPy() constructor
+session = InstaPy(username=insta_username, password=insta_password)
+session.login()
 
-InstaPy(username=insta_username, password=insta_password)\
-  .login()\
-  .set_upper_follower_count(limit=2500)\
-  .set_do_comment(True, percentage=10)\
-  .set_comments(['aMEIzing!', 'So much fun!!', 'Nicey!'])\
-  .set_dont_include(['friend1', 'friend2', 'friend3'])\
-  .set_dont_like(['pizza', 'girl'])\
-  .like_by_tags(['natgeo', 'world'], amount=100)\
-  .end()
+# set up all the settings
+session.set_upper_follower_count(limit=2500)
+session.set_do_comment(True, percentage=10)
+session.set_comments(['aMEIzing!', 'So much fun!!', 'Nicey!'])
+session.set_dont_include(['friend1', 'friend2', 'friend3'])
+session.set_dont_like(['pizza', 'girl'])
+
+# do the actual liking
+session.like_by_tags(['natgeo', 'world'], amount=100)
+
+# end the bot session
+session.end()
