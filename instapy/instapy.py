1c705badafe6e72439fad9e136cbbd2c2bb7c5b0
cclauss <cclauss@bluewin.ch>
2017-10-05 10:47:00 +0200

    user_name --> username

--- instapy/instapy.py
+++ instapy/instapy.py
@@ -706,7 +706,7 @@ class InstaPy:
             following = randint(0, 100) <= self.follow_percentage
             if (self.do_follow and username not in self.dont_include
                     and checked_img and following
-                    and self.follow_restrict.get(user_name, 0) < self.follow_times):
+                    and self.follow_restrict.get(username, 0) < self.follow_times):
                 followed += follow_user(self.browser, self.follow_restrict, self.username, username)
             else:
                 print('--> Not following')
