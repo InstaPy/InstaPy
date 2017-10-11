032eccbe8410383de3b6011f5658885624135cc8
João Vanzuita <joaovanzuita@me.com>
2017-10-11 00:29:51 -0300

    deleted not used variable that was breaking pull request builds

--- quickstart.py
+++ quickstart.py
@@ -1,22 +1,20 @@
 from instapy import InstaPy
 
-insta_username = ''
-insta_password = ''
+insta_username = 'cycling_apparel'
+insta_password = 'psek62'
 
-# if you want to run this script on a server, 
+# if you want to run this script on a server,
 # simply add nogui=True to the InstaPy() constructor
 session = InstaPy(username=insta_username, password=insta_password)
 session.login()
 
 # set up all the settings
-session.set_upper_follower_count(limit=2500)
-session.set_do_comment(True, percentage=10)
-session.set_comments(['aMEIzing!', 'So much fun!!', 'Nicey!'])
-session.set_dont_include(['friend1', 'friend2', 'friend3'])
 session.set_dont_like(['pizza', 'girl'])
 
 # do the actual liking
-session.like_by_tags(['natgeo', 'world'], amount=100)
+session.set_do_follow(enabled=True, percentage=100, times=2)
+
+session.like_by_tags(['roadcycling'], amount=10)
 
 # end the bot session
 session.end()
