8ac89e725d8f9d8c166f15494f2c40dcf047cd71
João Vanzuita <joaopaulo.mailfs@gmail.com>
2017-11-02 11:25:10 -0200

    catching all server callls, adding/updating statistics to csv file

--- instapy/util.py
+++ instapy/util.py
@@ -7,7 +7,6 @@ from selenium.common.exceptions import NoSuchElementException
 from tempfile import NamedTemporaryFile
 
 
-<<<<<<< HEAD
 def update_activity(action=None):
     """Record every Instagram server call (page load, content load, likes,
     comments, follows, unfollow)."""
