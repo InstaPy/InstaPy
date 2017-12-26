a8275990f8b98dd0357c00cd7ef97a76d48d4eb6
João Vanzuita <joaopaulo.mailfs@gmail.com>
2017-12-26 09:22:25 -0200

    deleted my password from PR

--- quickstart.py
+++ quickstart.py
@@ -1,7 +1,7 @@
 from instapy import InstaPy
 
-insta_username = 'cycling_apparel'
-insta_password = 'lx7qt5'
+insta_username = ''
+insta_password = ''
 
 # set headless_browser=True if you want to run InstaPy on a server
 try:
