855cd426e0d85db3c8c997aac135090877b52191
LoopForYou <34870243+LoopForYou@users.noreply.github.com>
2018-01-11 22:07:36 -0500

    telegram_util.py
    Notifies you (or someone else) that the bot has started, finished or is paused. This is better than using SMS because Telegram is free and easy to use.

---
+++ instapy/telegram_util.py
@@ -0,0 +1,46 @@
+#install python module with command
+# pip install python-telegram-bot
+
+import telegram
+from telegram.error import NetworkError, Unauthorized
+
+update_id = None
+
+
+def start():
+    """Run the bot."""
+    global update_id
+    # Telegram Bot Authorization Token
+    bot = telegram.Bot('<token>')
+    echo_start(bot)
+    
+def finish():
+    """Run the bot."""
+    global update_id
+    # Telegram Bot Authorization Token
+    bot = telegram.Bot('<token>')
+    echo_finish(bot)
+    
+def pause():
+    """Run the bot."""
+    global update_id
+    # Telegram Bot Authorization Token
+    bot = telegram.Bot('<token>')
+    echo_pause(bot)
+
+def echo_start(bot):
+	bot.send_message(-1001000003702, insta_username + ' started botting')
+	
+def echo_finish(bot):
+	bot.send_message(-1001000003702, insta_username + ' finished botting')
+	
+def echo_pause(bot):
+	bot.send_message(-10010000003702, insta_username + ' is pretending to be human and is idle for 1 hour. You can logon for an hour if you want.')
+#bot.send_message(<Channel ID>, what_ever_you_want_it_to_echo)
+	
+#To Create a bot on Telegram follow these steps: https://core.telegram.org/bots#6-botfather (https://telegram.me/botfather)
+#Yes, you actually have to go into Telegram and talk to a Bot to create a bot. The Father of Bots that is...
+#Then create a channel with your real account (not the bot account)
+#Add the bot to the channel and to find the channel ID you will need to use this API Call
+# https://api.telegram.org/bot<token>/getUpdates
+#The Channel ID will start with a '-' (minus sign)
