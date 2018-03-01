32ce26225ea6cfde20f5c4de8a13f35bf3bfd8f2
Tim Großmann <t.grossmann1994@web.de>
2018-03-01 08:04:19 -0800

    adds 4th argument to log_...

--- instapy/unfollow_util.py
+++ instapy/unfollow_util.py
@@ -151,11 +151,11 @@ def unfollow(browser,
                     else:
 						# this user found in our list of unfollow but is not followed
                         if follow_button.text != 'Follow':
-                            log_uncertain_unfollowed_pool(username, person, logger)
+                            log_uncertain_unfollowed_pool(username, person, logger, logfolder)
                         delete_line_from_file('{0}{1}_followedPool.csv'.format(logfolder, username),
                                               person + ",\n", logger)
                         # save any unfollowed person
-                        log_record_all_unfollowed(username, person, logger)
+                        log_record_all_unfollowed(username, person, logger, logfolder)
 
                         logger.warning(
                             '--> Cannot Unfollow From InstaPy {}'
