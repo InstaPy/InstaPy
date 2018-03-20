8ae61b2f28c3c4d4775aa0cd347bca9285ddf53a
timo doersching <timo@doersching.net>
2018-03-20 23:11:30 +0100

    Daily log of follower from Instagram User that your are following/like pics

--- instapy/settings.py
+++ instapy/settings.py
@@ -8,3 +8,4 @@ class Settings:
     log_location = os.path.join(BASE_DIR, 'logs')
     database_location = os.path.join(BASE_DIR, 'db', 'instapy.db')
     chromedriver_location = os.path.join(BASE_DIR, 'assets', 'chromedriver')
+    log_profile_statistic = True
