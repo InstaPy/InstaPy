cff03ddb0bf344cbd7caf8f18eb80539273470fc
Jaco Jansen <Jaco.Jansen@iress.com.au>
2018-03-21 21:44:10 +0100

    Changed logfolder to format

--- instapy/instapy.py
+++ instapy/instapy.py
@@ -87,7 +87,7 @@ class InstaPy:
         self.nogui = nogui
         self.logfolder = Settings.log_location + os.path.sep
         if multi_logs is True:
-            self.logfolder = os.path.join(Settings.log_location, self.username) + os.path.sep
+            self.logfolder = '{}/{}/'.format(Settings.log_location, self.username)
         if not os.path.exists(self.logfolder):
             os.makedirs(self.logfolder)
 
