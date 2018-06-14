bfc27203b503145162402d730037314a6fb4af0e
ilmetu <ilmetu@gmail.com>
2018-06-14 20:13:26 +0200

    use different chromedriver for os environment
    if you need more then one chromedriver (for development purpose) you can rename it as :
    
    chromedriver_linux
    chromedriver_osx
    chromedriver_windows
    
    and you can put all in assets directory

--- instapy/settings.py
+++ instapy/settings.py
@@ -1,13 +1,34 @@
 import os
 
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+def os_env():
+    from sys import platform
+    if platform == "linux" or platform == "linux2":
+        return 'linux'
+    elif platform == "darwin":
+        return 'osx'
+    elif platform == "win32":
+        return 'windows'
 
 class Settings:
     log_location = os.path.join(BASE_DIR, 'logs')
     database_location = os.path.join(BASE_DIR, 'db', 'instapy.db')
+
     chromedriver_location = os.path.join(BASE_DIR, 'assets', 'chromedriver')
+
+    if os_env() == 'osx':
+        chromedriver_location = os.path.join(BASE_DIR, 'assets', 'chromedriver_osx')
+
+    if os_env() == 'linux':
+        chromedriver_location = os.path.join(BASE_DIR, 'assets', 'chromedriver_linux')
+
+    if os_env() == 'windows':
+        chromedriver_location = os.path.join(BASE_DIR, 'assets', 'chromedriver_windows')
+
+    if not os.path.exists(chromedriver_location):
+        chromedriver_location = os.path.join(BASE_DIR, 'assets', 'chromedriver')
+
     chromedriver_min_version = 2.36
     # Set a logger cache outside the InstaPy object to avoid re-instantiation issues
     loggers = {}
