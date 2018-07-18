1ffc0f9ae0d484966545edad24fd75424beb947a
Joan Roig <joanroigsegura@gmail.com>
2018-07-18 22:15:57 +0200

    Delete user if followed and when trying to unfollow it doesn't exist
    Delete user if followed with InstaPy, and when trying to unfollow it doesn't exist and remains in the followedPool.

--- instapy/unfollow_util.py
+++ instapy/unfollow_util.py
@@ -258,6 +258,8 @@ def unfollow(browser,
                             '--> Unfollow error with {},'
                             ' maybe no longer exists...'
                                 .format(person.encode('utf-8')))
+                        delete_line_from_file('{0}{1}_followedPool.csv'.format(logfolder, username), person + ",\n", logger)
+
                         continue
 
                     if following:
