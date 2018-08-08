c7720bee026b8f8d44da151ab69b7f93dfa7f998
Marco Antonietti <marco.mokastyle@gmail.com>
2018-08-08 02:14:41 +0200

    Removed test comment

--- instapy/instapy.py
+++ instapy/instapy.py
@@ -69,7 +69,6 @@ class InstaPyError(Exception):
 
 class InstaPy:
     """Class to be instantiated to use the script"""
-    # comment to test LE
     def __init__(self,
                  username=None,
                  password=None,
