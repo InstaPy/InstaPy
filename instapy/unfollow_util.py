a96bc67494ec87f1c786c483760d84f4cb1306c2
Qulu <shahriyarguliyev@hotmail.com>
2018-08-17 00:49:19 +0400

    Prevent indexing error once `quick_username` comes out empty

--- instapy/unfollow_util.py
+++ instapy/unfollow_util.py
@@ -676,7 +676,8 @@ def get_users_through_dialog(browser,
                 quick_index = random.randint(0, len(buttons)-1)
                 quick_button = buttons[quick_index]
                 quick_username = dialog_username_extractor(quick_button)
-                if quick_username[0] not in simulated_list:
+
+                if quick_username and quick_username[0] not in simulated_list:
                     quick_follow = follow_through_dialog(browser,
                                                          login,
                                                          quick_username,
