179dca14ccb0760d69896333543c956caf704d91
Charles Cai <caicongchen@gmail.com>
2018-08-21 19:16:23 -0400

    remove unnecessary change

--- quickstart.py
+++ quickstart.py
@@ -6,8 +6,8 @@ from selenium.common.exceptions import NoSuchElementException
 
 from instapy import InstaPy
 
-insta_username = 'comeshareit'
-insta_password = 'WATWATWAT'
+insta_username = ''
+insta_password = ''
 
 # set headless_browser=True if you want to run InstaPy on a server
 
