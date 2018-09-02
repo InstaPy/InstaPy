49eaee65af7cdb54ecc3d646c43aaadb81eb600a
Yannik <InfinityWebMe@users.noreply.github.com>
2018-09-02 14:35:42 +0200

    Proposition - Add date in general log filename
    Hello,
    
    here a proposition to avoid having a big log file. 
    
    What do you think ?

--- instapy/instapy.py
+++ instapy/instapy.py
@@ -205,7 +205,7 @@ class InstaPy:
             # initialize and setup logging system for the InstaPy object
             logger = logging.getLogger(__name__)
             logger.setLevel(logging.DEBUG)
-            file_handler = logging.FileHandler('{}general.log'.format(self.logfolder))
+            file_handler = logging.FileHandler('{0}{1}-general.log'.format(self.logfolder, datetime.now().strftime("%Y-%m-%d")))
             file_handler.setLevel(logging.DEBUG)
             extra = {"username": self.username}
             logger_formatter = logging.Formatter('%(levelname)s [%(asctime)s] [%(username)s]  %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
