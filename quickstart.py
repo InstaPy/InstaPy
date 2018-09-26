1798a8d06dcfbc74df460f527c354f537748c79b
Tim Großmann <t.grossmann1994@web.de>
2018-09-26 22:56:05 +0200

    modify minimal quickstart file

--- quickstart.py
+++ quickstart.py
@@ -10,37 +10,25 @@ insta_username = ''
 insta_password = ''
 
 # get an InstaPy session!
+# set headless_browser=True to run InstaPy in the background
 session = InstaPy(username=insta_username,
                   password=insta_password,
-                   headless_browser=False,
-                    multi_logs=True)
+                  headless_browser=False)
 
 
 with smart_run(session):
     """ Activity flow """
     # settings
-    session.set_quota_supervisor(enabled=True,
-                                  sleep_after=["likes"],
-                                   peak_likes=(66, 700),
-                                   peak_server_calls=(None, 4500))
     session.set_relationship_bounds(enabled=True,
-                                     potency_ratio=1.07,
                                       delimit_by_numbers=True,
                                        max_followers=4590,
-                                       max_following=5555,
                                         min_followers=45,
                                         min_following=77)
+    
     session.set_dont_include(["friend1", "friend2", "friend3"])
     session.set_dont_like(["pizza", "#store"])
-    session.set_do_comment(True, percentage=14)
-    session.set_comments(["Brilliant",
-                          "So wonderful!",
-                          "Extraordinarily good.",
-                          "A way so marvelous..",
-                          "Literally a breathtaking capture!",
-                          "You have a gallery so neat!",
-                          "You are a sharp shooter, @{}!"])
-
+    
+    
     # actions
     session.like_by_tags(["natgeo"], amount=10)
 
