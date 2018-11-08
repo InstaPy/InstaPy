5a962a582f5b71b01bc9bd2abbd676b4b8ed1e68
Tim Großmann <t.grossmann1994@web.de>
2018-11-08 14:51:12 +0100

    Quick fix for a bug off cc3b4f0 by @uluQulu

--- instapy/util.py
+++ instapy/util.py
@@ -131,13 +131,17 @@ def validate_username(browser,
 
     logfolder = logfolder = '{0}{1}{2}{1}'.format(
             Settings.log_location, os.path.sep, own_username)
-    with open("{}blacklist.csv".format(logfolder), 'rt') as f:
-     reader = csv.reader(f, delimiter=',')
-     for row in reader:
-          for field in row:
-              if field == username:
-                          logger.info('Username in BlackList: {} '.format(username))
-                          return False, "---> {} is in blacklist  ~skipping user\n".format(username)
+    
+    blacklist_file = "{}blacklist.csv".format(logfolder)
+    blacklist_file_exists = os.path.isfile(blacklist_file)
+    if blacklist_file_exists:
+        with open("{}blacklist.csv".format(logfolder), 'rt') as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+            for field in row:
+                if field == username:
+                    logger.info('Username in BlackList: {} '.format(username))
+                    return False, "---> {} is in blacklist  ~skipping user\n".format(username)
 
     """Checks the potential of target user by relationship status in order to delimit actions within the desired boundary"""
     if potency_ratio or delimit_by_numbers and (max_followers or max_following or min_followers or min_following):
