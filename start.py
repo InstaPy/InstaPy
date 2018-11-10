83ab8d572428f1dbb2f42b476f2c92e4a4efb5cd
gdurandvadas <gdurandvadas@gmail.com>
2018-11-10 14:15:07 +0100

    Adding random comments

--- start.py
+++ start.py
@@ -1,168 +1,178 @@
 import os
 import yaml
+import random
 from instapy import InstaPy
 from instapy.util import smart_run
 
 # Importing datafile
 current_path = os.path.abspath(os.path.dirname(__file__))
-data = yaml.safe_load(open("%s/data.yaml" %(current_path) ))
+data = yaml.safe_load(open('%s/data.yaml' %(current_path) ))
 
-username = os.environ['IG_USER']
-password = os.environ['IG_PASS']
-skip_friends_interaction = os.environ['SKIP_FRIENDS_INTERACTION']
-skip_hashtags_interaction = os.environ['SKIP_HASHTAGS_INTERACTION']
-skip_locations_interaction = os.environ['SKIP_LOCATIONS_INTERACTION']
-skip_follow_unfollow = os.environ['SKIP_FOLLOW_UNFOLLOW']
+skip_friends_interaction = data['skip']['friends_interaction']
+skip_hashtags_interaction = data['skip']['hashtags_interaction']
+skip_locations_interaction = data['skip']['locations_interaction']
+skip_follow_unfollow = data['skip']['follow_unfollow']
 
 # Setting data
-creds = data["creds"]
-hashtags = data["tags"]
-exceptions = data["exceptions"]
-friends = data["friends"]
-brands = data["brands"]
-interests = data["interests"]
-artists = data["artists"]
-netherlands = data["netherlands"]
+creds = data['creds']
+hashtags = data['tags']
+exceptions = data['exceptions']
+friends = data['friends']
+brands = data['brands']
+interests = data['interests']
+artists = data['artists']
+netherlands = data['netherlands']
 
 # Comments
-comments = [
-  u"🔥🔥🔥",
-  u"✨✨✨✨",
-  u"⚡️️️️️️️⚡️️️️️️️⚡️️️️️️️",
-  u"🙌🏼 🙌🏼",
-  u"😻"
+constructors = [
+  u'🔥',
+  u'✨',
+  u'⚡️',
+  u'🙌🏼',
+  u'😻',
+  u'💪🏼',
+  u'😎',
+  u'♥️',
+  u'💫'
 ]
 
+comments = []
 
-###############################
-# Interact with close friends #
-# - likes
-if skip_friends_interaction == "yes":
-  print('⚡️ Skipping liking friends posts')
-else:
-  print('❤️ Showing friends some love')
-  friend_list = InstaPy(username=creds['user'], password=creds['pass'],
-                        selenium_local_session=False, disable_image_load=True)
-  friend_list.set_selenium_remote_session(selenium_url='http://selenium:4444/wd/hub')
-  friend_list.login()
-  friend_list.set_skip_users(skip_private=False)
-  friend_list.set_relationship_bounds(enabled=True, potency_ratio=0.4)
-  friend_list.set_do_like(True, percentage=80)
-  friend_list.interact_by_users(friends, amount=2, randomize=False)
-  friend_list.end()
+for x in range(20):
+  comment = []
+  comment = random.sample(constructors, random.randint(1,5))
+  comments.append(u''.join(comment))
 
+# print('Friends Interaction')
+# ###############################
+# # Interact with close friends #
+# # - likes
+# if skip_friends_interaction:
+#   print('⚡️ Skipping liking friends posts')
+# else:
+#   print('❤️ Showing friends some love')
+#   friend_list = InstaPy(username=creds['user'], password=creds['pass'],
+#                         selenium_local_session=False, disable_image_load=True)
+#   friend_list.set_selenium_remote_session(selenium_url='http://selenium:4444/wd/hub')
+#   friend_list.login()
+#   friend_list.set_skip_users(skip_private=False)
+#   friend_list.set_relationship_bounds(enabled=True, potency_ratio=0.4)
+#   friend_list.set_do_like(True, percentage=80)
+#   friend_list.interact_by_users(friends, amount=2, randomize=False)
+#   friend_list.end()
 
-#########################################
-# Interact with high profiles by hashtag #
-# - likes
-# - comments
-bot = InstaPy(username=creds['user'], password=creds['pass'],
-              selenium_local_session=False, disable_image_load=True)
-bot.set_selenium_remote_session(selenium_url='http://selenium:4444/wd/hub')
+# print('🌋 ⛏  📊')
+# #########################################
+# # Interact with high profiles by hashtag #
+# # - likes
+# # - comments
+# bot = InstaPy(username=creds['user'], password=creds['pass'],
+#               selenium_local_session=False, disable_image_load=True)
+# bot.set_selenium_remote_session(selenium_url='http://selenium:4444/wd/hub')
 
-# Don't bother friends
-bot.set_dont_include(friends)
+# # Don't bother friends
+# bot.set_dont_include(friends)
 
-# Don't get banned 
-bot.set_quota_supervisor(enabled=True, sleep_after=["likes", "comments_d", "follows", "unfollows", "server_calls_h"],
-                        sleepyhead=True, stochastic_flow=True, notify_me=True, peak_likes=(57, 585),
-                        peak_comments=(21, 182), peak_follows=(48, None), peak_unfollows=(35, 402),
-                        peak_server_calls=(None, 4700))
+# # Don't get banned 
+# bot.set_quota_supervisor(enabled=True, sleep_after=['likes', 'comments_d', 'follows', 'unfollows', 'server_calls_h'],
+#                         sleepyhead=True, stochastic_flow=True, notify_me=True, peak_likes=(57, 585),
+#                         peak_comments=(21, 182), peak_follows=(48, None), peak_unfollows=(35, 402),
+#                         peak_server_calls=(None, 4700))
 
-# Login
-bot.login()
+# # Login
+# bot.login()
 
-# Exceptions
-bot.set_dont_like(exceptions)
+# # Exceptions
+# bot.set_dont_like(exceptions)
 
-# Comment
-bot.set_comments(comments)
-bot.set_do_comment(enabled=True, percentage=50)
+# # Comment
+# bot.set_comments(comments)
+# bot.set_do_comment(enabled=True, percentage=50)
 
-if skip_hashtags_interaction == "yes":
-  print('⚡️ Skipping interaction based on hashtags')
-else:
-  print('🏗 Building hashtags network')
-  # Smart hashtags
-  bot.set_smart_hashtags(hashtags, limit=4, sort='top', log_tags=True)
+# if skip_hashtags_interaction:
+#   print('⚡️ Skipping interaction based on hashtags')
+# else:
+#   print('🏗 Building hashtags network')
+#   # Smart hashtags
+#   bot.set_smart_hashtags(hashtags, limit=4, sort='top', log_tags=True)
 
-  # Setting bounds
-  bot.set_relationship_bounds(enabled=True, potency_ratio=1.5, delimit_by_numbers=True,
-                              max_followers=99999999, max_following=5000,
-                              min_followers=700, min_following=0, min_posts=10)
+#   # Setting bounds
+#   bot.set_relationship_bounds(enabled=True, potency_ratio=1.5, delimit_by_numbers=True,
+#                               max_followers=99999999, max_following=5000,
+#                               min_followers=700, min_following=0, min_posts=10)
 
-  # Like
-  bot.like_by_tags(amount=20, use_smart_hashtags=True)
-
-
-########################
-# Interact by location #
-# - likes
-if skip_friends_interaction == "yes":
-  print('⚡️ Skipping interaction based on locations')
-else:
-  print('🏗  Building locations network')
-  # Netherlands
-  bot.like_by_locations(netherlands, amount=2)
+#   # Like
+#   bot.like_by_tags(amount=20, use_smart_hashtags=True)
 
 
-######################################
-# Follow/Unfollow flow plus interact  #
-# - likes
-# - comments
-if skip_follow_unfollow == "yes":
-  print('⚡️ Skipping follow/unfollow')
-else:
-  print('🏔⛏ Mining followers')
-  bot.set_relationship_bounds(enabled=True, potency_ratio=1, delimit_by_numbers=True,
-                              min_followers=250, min_following=50, min_posts=30)
+# ########################
+# # Interact by location #
+# # - likes
+# if skip_friends_interaction:
+#   print('⚡️ Skipping interaction based on locations')
+# else:
+#   print('🏗  Building locations network')
+#   # Netherlands
+#   bot.like_by_locations(netherlands, amount=2)
 
-  ## Don't be an asshole config ##
 
-  # Setting blacklist
-  bot.set_blacklist(enabled=True, campaign='blacklist')
+# ######################################
+# # Follow/Unfollow flow plus interact  #
+# # - likes
+# # - comments
+# if skip_follow_unfollow:
+#   print('⚡️ Skipping follow/unfollow')
+# else:
+#   print('🏔⛏ Mining followers')
+#   bot.set_relationship_bounds(enabled=True, potency_ratio=1, delimit_by_numbers=True,
+#                               min_followers=250, min_following=50, min_posts=30)
 
-  # Dont bother nice people
-  bot.set_dont_include(friends+brands+interests+artists)
+#   ## Don't be an asshole config ##
 
-  # Exclude private or accounts without profile picture
-  bot.set_skip_users(skip_private=True, private_percentage=100,
-                    skip_no_profile_pic=True, skip_business=False,)
+#   # Setting blacklist
+#   bot.set_blacklist(enabled=True, campaign='blacklist')
 
-  # Set user interaction
-  bot.set_do_like(enabled=True, percentage=100)
-  bot.set_do_comment(enabled=True, percentage=100)
-  bot.set_comments(comments)
+#   # Dont bother nice people
+#   bot.set_dont_include(friends+brands+interests+artists)
 
-  # Set how much interaction
-  bot.set_user_interact(amount=1, randomize=False, percentage=50)
-  bot.set_simulation(enabled=True)
+#   # Exclude private or accounts without profile picture
+#   bot.set_skip_users(skip_private=True, private_percentage=100,
+#                     skip_no_profile_pic=True, skip_business=False,)
 
-  ## Group Nº1 ##
-  # FOLLOW
-  # brands
-  bot.follow_user_followers(brands, amount=100, randomize=False, interact=True, sleep_delay=600)
+#   # Set user interaction
+#   bot.set_do_like(enabled=True, percentage=100)
+#   bot.set_do_comment(enabled=True, percentage=100)
+#   bot.set_comments(comments)
 
-  # UNFOLLOW
-  # People that didn't followed back in the last 48hs
-  bot.set_blacklist(enabled=False, campaign='blacklist')
-  bot.unfollow_users(amount=90, InstapyFollowed=(True, "nonfollowers"), style="FIFO", unfollow_after=12*60*60, sleep_delay=601)
+#   # Set how much interaction
+#   bot.set_user_interact(amount=1, randomize=False, percentage=50)
+#   bot.set_simulation(enabled=True)
 
-  ## Group Nº2 ##
-  # FOLLOW
-  # interests+artists
-  bot.set_blacklist(enabled=True, campaign='blacklist')
-  bot.follow_user_followers(interests+artists, amount=100, randomize=False, interact=True, sleep_delay=600)
+#   ## Group Nº1 ##
+#   # FOLLOW
+#   # brands
+#   bot.follow_user_followers(brands, amount=100, randomize=False, interact=True, sleep_delay=600)
 
-  # UNFOLLOW
-  # People that didn't followed back in the last 48hs
-  bot.set_blacklist(enabled=False, campaign='blacklist')
-  bot.unfollow_users(amount=90, InstapyFollowed=(True, "nonfollowers"), style="FIFO", unfollow_after=12*60*60, sleep_delay=601)
+#   # UNFOLLOW
+#   # People that didn't followed back in the last 48hs
+#   bot.set_blacklist(enabled=False, campaign='blacklist')
+#   bot.unfollow_users(amount=90, InstapyFollowed=(True, 'nonfollowers'), style='FIFO', unfollow_after=12*60*60, sleep_delay=601)
 
-  # CLEANUP
-  bot.unfollow_users(amount=500, InstapyFollowed=(True, "all"), style="FIFO", unfollow_after=24*60*60, sleep_delay=601) 
+#   ## Group Nº2 ##
+#   # FOLLOW
+#   # interests+artists
+#   bot.set_blacklist(enabled=True, campaign='blacklist')
+#   bot.follow_user_followers(interests+artists, amount=100, randomize=False, interact=True, sleep_delay=600)
 
-# Closing sessions
-bot.end()
+#   # UNFOLLOW
+#   # People that didn't followed back in the last 48hs
+#   bot.set_blacklist(enabled=False, campaign='blacklist')
+#   bot.unfollow_users(amount=90, InstapyFollowed=(True, 'nonfollowers'), style='FIFO', unfollow_after=12*60*60, sleep_delay=601)
+
+#   # CLEANUP
+#   bot.unfollow_users(amount=500, InstapyFollowed=(True, 'all'), style='FIFO', unfollow_after=24*60*60, sleep_delay=601) 
+
+# # Closing sessions
+# bot.end()
 
 
