19f70025fdfd6804837af57246eebf3ca41fff23
sionking <sionking@gmail.com>
2018-12-08 21:00:42 +0200

    remove un

--- instapy/instapy.py
+++ instapy/instapy.py
@@ -39,7 +39,7 @@ from .login_util import login_user
 from .settings import Settings
 from .print_log_writer import log_follower_num
 from .print_log_writer import log_following_num
-from .print_log_writer import get_log_time
+
 from .time_util import sleep
 from .time_util import set_sleep_percentage
 from .util import get_active_users
