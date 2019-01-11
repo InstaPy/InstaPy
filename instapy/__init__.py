ae5d9a894d712085392414c5d6b33be5e07547bd
Qulu <shahriyarguliyev@hotmail.com>
2019-01-12 03:30:57 +0400

    Update __init__.py file with new package imports & store version info
    Having version information stored here will help to get it as ```instapy.__version__```
    
    Also, it will ease changing version number each time from here rather than from setup.py[setup()].

--- instapy/__init__.py
+++ instapy/__init__.py
@@ -1,3 +1,12 @@
 # flake8: noqa
-from .settings import Settings
+
 from .instapy import InstaPy
+from .util import smart_run
+from .settings import Settings
+from .file_manager import set_workspace
+from .file_manager import get_workspace
+
+
+# __variables__ with double-quoted values will be available in setup.py
+__version__ = "0.0.0"
+
