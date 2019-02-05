InstaPy/InstaPy

commit 8e4fb1455ded426a1fc57407d9d08e4a56b6a308

Fix for scrollIntoView error
line 740 - updated xpath

instapy/unfollow_util.py
@@ -737,7 +737,7 @@ def get_users_through_dialog(browser,
     sc_rolled = 0
 
     # find dialog box
-    dialog_address = "//body/div[2]/div/div/div[2]"
+    dialog_address = "//body/div[2]/div"
     dialog = browser.find_element_by_xpath(dialog_address)
 
     # scroll to end of follower list to initiate first load which hides the

commit 3951f858e684487c358364ebd8fff37469c610dc

fixing bug introduced by https://github.com/timgrossmann/InstaPy/commit/8e4fb1455ded426a1fc57407d9d08e4a56b6a308

instapy/unfollow_util.py
@@ -737,7 +737,7 @@ def get_users_through_dialog(browser,
     sc_rolled = 0
 
     # find dialog box
-    dialog_address = "//body/div[2]/div"
+    dialog_address = "//body/div[2]/div/div[2]"
     dialog = browser.find_element_by_xpath(dialog_address)
 
     # scroll to end of follower list to initiate first load which hides the