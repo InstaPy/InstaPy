513707483f40e5bd2a6bdc6fc654fac2bf300b84
Qulu <shahriyarguliyev@hotmail.com>
2019-02-05 20:34:57 +0400

    Fix "Failed to load desired amount of users!" issue.

--- instapy/unfollow_util.py
+++ instapy/unfollow_util.py
@@ -738,6 +738,7 @@ def get_users_through_dialog(browser,
 
     # find dialog box
     dialog_address = "//body/div[2]/div"
+    dialog_address = "//body/div[2]/div/div[2]"
     dialog = browser.find_element_by_xpath(dialog_address)
 
     # scroll to end of follower list to initiate first load which hides the
