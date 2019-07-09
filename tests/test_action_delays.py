1a442a95d7fd6e384e484db38c5a2cb5657a35d6
Felix Breuer <webmaster@scriptworld.net>
2019-07-09 10:19:45 +0200

    change to single quote to trigger codacy rebuild

--- tests/test_action_delays.py
+++ tests/test_action_delays.py
@@ -2,7 +2,7 @@ from instapy import util
 
 
 def test_default_values_returned():
-    assert util.get_action_delay("like") == 2
-    assert util.get_action_delay("comment") == 2
-    assert util.get_action_delay("follow") == 3
-    assert util.get_action_delay("unfollow") == 10
+    assert util.get_action_delay('like') == 2
+    assert util.get_action_delay('comment') == 2
+    assert util.get_action_delay('follow') == 3
+    assert util.get_action_delay('unfollow') == 10
