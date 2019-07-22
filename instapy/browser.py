e9da89eb0848415932ddea115bc55f9892ce7b12
felixbreuer <hi@felixbreuer.me>
2019-07-22 18:56:33 +0200

    hide infobars

--- instapy/browser.py
+++ instapy/browser.py
@@ -101,6 +101,7 @@ def set_selenium_local_session(proxy_address,
         chrome_options.add_argument('--lang=en-US')
         chrome_options.add_argument('--disable-setuid-sandbox')
         chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-infobars')
 
         # this option implements Chrome Headless, a new (late 2017)
         # GUI-less browser. chromedriver 2.9 and above required
