43575b3cf4c5b1e4d79927ce8fbb488c9061bdea
felixbreuer <hi@felixbreuer.me>
2019-07-26 16:51:58 +0200

    add page delay to self

--- instapy/instapy.py
+++ instapy/instapy.py
@@ -143,6 +143,7 @@ class InstaPy:
                 raise InstaPyError("The 'nogui' parameter isn't supported on Windows.")
 
         self.browser = None
+        self.page_delay = page_delay
         self.bypass_with_mobile = bypass_with_mobile
         self.disable_image_load = disable_image_load
 
