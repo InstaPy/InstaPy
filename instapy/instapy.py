3e831dd4258d93c43a57756e5af11a57bb249fdb
notadrian <adrian.stein@socialyze.io>
2019-08-13 22:25:51 +0200

    fix set_smart_hashtags with sort type random

--- instapy/instapy.py
+++ instapy/instapy.py
@@ -700,7 +700,10 @@ class InstaPy:
                         self.smart_hashtags.append(item["tag"])
 
                 elif sort == "random":
-                    random_tags = random.sample(data["results"], limit)
+                    if len(data['results']) < limit:
+                        random_tags = random.sample(data['results'], len(data['results']))
+                    else:
+                        random_tags = random.sample(data["results"], limit)
                     for item in random_tags:
                         self.smart_hashtags.append(item["tag"])
 
