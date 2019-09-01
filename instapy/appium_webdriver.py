cbbeb700b36e9e3593b15b6c13a38ad05b70da7e
Diego Fabiano <dfabiano@mail.usf.edu>
2019-09-01 13:47:39 -0400

    appium webdriber fixes & additions

--- instapy/appium_webdriver.py
+++ instapy/appium_webdriver.py
@@ -3,13 +3,14 @@ Class to define everything needed to work with Appium
 """
 
 from appium import webdriver
+import subprocess
 
-class AppiumWebDriver(InstaPyLog):
+class AppiumWebDriver():
     """
     Appium WebDriver class
     """
 
-    def __init__(self, devicename: str=''):
+    def __init__(self, devicename: str='',devicetimeout: int=600):
 
         super().__init__()
 
@@ -27,6 +28,7 @@ class AppiumWebDriver(InstaPyLog):
             __desired_caps['fullReset'] = False
             __desired_caps['unicodeKeyboard'] = True
             __desired_caps['resetKeyboard'] = True
+            __desired_caps['newCommandTimeout'] = devicetimeout
 
             try:
                 _driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
@@ -40,11 +42,23 @@ class AppiumWebDriver(InstaPyLog):
     def driver(self):
         return _driver
 
-    def _adb_list_devices(self):
+    def _adb_list_devices(self,adb_path: str=''):
         """
         protected function to check the current running simulators
         :return:
         """
+
+        console_output = subprocess.check_output([adb_path, 'devices']).splitlines()
+        devices = []
+
+        print('List of available devices:')
+        for line in console_output[1:]:
+            line = line.decode("utf-8")
+            if line != '':
+                device = line.split('\t')[0] # Not sure if \\ is only for windows; have to check
+                devices.append(device)
+                print(device)
+        return devices
 
 
     def find_element_by_xpath(self,xpath: str=''):
@@ -54,6 +68,14 @@ class AppiumWebDriver(InstaPyLog):
         :return:
         """
         return self.driver.find_element_by_xpath(xpath)
+
+    def find_element_by_id(self, resource_id: str=''):
+        """
+        wrapper for find_element_by_id
+        :param resource_id:
+        :return:
+        """
+        return self.driver.find_element_by_id(uiautomator)
 
     def find_element_by_uiautomator(self, uiautomator: str=''):
         """
@@ -70,7 +92,3 @@ class AppiumWebDriver(InstaPyLog):
         :return:
         """
         webelem.click()
-
-
-
-    
