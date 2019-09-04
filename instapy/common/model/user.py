ae8a97c192b9855e622fa023c1a320a53b31670f
christophe-duc <christophe@twilight-corp.com>
2019-09-04 19:21:16 -0300

    normalized user class

--- instapy/common/model/user.py
+++ instapy/common/model/user.py
@@ -4,15 +4,18 @@ User model for interactions user attributes and perform action on users
 
 
 class User(object):
-    def __init__(self, username=None):
-        # text=None, post_count=None, follower_count=None, following_count=None):
-        self.username = username
+    def __init__(self, username=None, post_count=None, follower_count=None, following_count=None, full_name=None, bio=None ):
+        """
+        init
+        """
 
-        self._posts = None
-        self._following = None
-        self._followers = None
-        self._full_name_header = None
-        self._description = None
+        self.username = username
+        self._post_count = post_count
+        self._follower_count = follower_count
+        self._following_count = following_count
+        self._full_name = full_name
+        self._bio = bio
+
 
     # Used for working with sets
     def __hash__(self):
@@ -34,40 +37,107 @@ class User(object):
         return repr(self)
 
     @property
-    def posts(self):
+    def post_count(self):
         """
         Getter for post_count (self.post_count will call this function automatically)
         :return:
         """
 
-        return self._posts
+        if self._post_count is None:
+            self.get_post_count()
+
+        return self._post_count
 
     @property
-    def followers(self):
+    def follower_count(self):
         """
         Getter for follower_count (self.follower_count will call this function automatically)
         :return:
         """
 
-        return self._followers
+        if self._follower is None:
+            self.get_follower()
+
+        return self._follower
 
     @property
-    def following(self):
+    def following_count(self):
         """
         Getter for following_count (self.following_count will call this function automatically)
         :return:
         """
 
+        if self._following is None:
+            self.get_following_count()
+
         return self._following
 
-    def populate(self,posts,following,followers,header,desc):
+    @property
+    def bio(self):
+        """
+        Getter for the bio
+        :return:
+        """
+
+        if self._bio is None:
+            self.get_bio()
+
+        return self._bio
+
+    @property
+    def full_name(self):
+        """
+        Getter for the header description
+        :return:
+        """
+
+        if self._full_name is None:
+            self.get_full_name()
+
+        return self._full_name
+
+    def populate(self,post_count=0,following_count=0,follower_count=0,full_name="",bio=""):
         """
          load the data into the User object
-         :param session:
-         :return:
          """
-         self._posts = posts
-         self._followers = followers
-         self._following = following
-         self._full_name_header = header
-         self._description = desc
+
+        self._post_count = post_count
+        self._follower_count = follower_count
+        self._following_count = following_count
+        self._full_name = full_name
+        self._bio = bio
+
+    def get_post_count(self):
+        """
+        Abstract, to be implemented by the driver
+        :return:
+        """
+        pass
+
+    def get_follower_count(self):
+        """
+        Abstract to be implemented by the driver
+        :return:
+        """
+        pass
+
+    def get_following_count(self):
+        """
+        Abstract to be implemented by the driver
+        :return:
+        """
+        pass
+
+    def get_bio(self):
+        """
+        Abstract to be implemented by the driver
+        :return:
+        """
+        pass
+
+    def get_full_name(self):
+        """
+        Abstract to be implemented by the driver
+        :return:
+        """
+        pass
