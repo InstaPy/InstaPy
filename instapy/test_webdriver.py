8f8ee5b3fbf3a673cd12402c1cbe272aace0ea9b
christophe-duc <christophe@twilight-corp.com>
2019-09-04 17:50:40 -0300

    fixed imports

--- instapy/test_webdriver.py
+++
@@ -1,8 +0,0 @@
-from appium_webdriver import AppiumWebDriver
-from common.model import user as usr
-from drivers.appium_endpoints import user_actions
-
-driver = AppiumWebDriver(devicename='emulator-5554')
-user = usr()
-find_and_populate_user(driver,user)
-print(user)
---
+++ tests/appium/test_webdriver.py
@@ -0,0 +1,8 @@
+from instapy.drivers import AppiumWebDriver
+from instapy.common.model import user as usr
+#from instapy.drivers.appium_endpoints import user_actions
+
+driver = AppiumWebDriver(devicename='emulator-5554')
+user = usr()
+find_and_populate_user(driver,user)
+print(user)
