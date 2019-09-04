575758512532515e3e3e31db4b93f1beb4478eae
christophe-duc <christophe@twilight-corp.com>
2019-09-04 19:51:47 -0300

    correct webdriver test

--- tests/appium/test_webdriver.py
+++ tests/appium/test_webdriver.py
@@ -1,8 +1,9 @@
 from instapy.drivers import AppiumWebDriver
 from instapy.common.model import user as usr
-#from instapy.drivers.appium_endpoints import user_actions
 
-driver = AppiumWebDriver(devicename='emulator-5554')
+# from instapy.drivers.appium_endpoints import user_actions
+
+driver = AppiumWebDriver(devicename="emulator-5554")
 user = usr()
-find_and_populate_user(driver,user)
+find_and_populate_user(driver, user)
 print(user)
