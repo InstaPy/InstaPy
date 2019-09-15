dafed5d993aea866ac20da47af300a9378d4308a
jm-willy <53955471+jm-willy@users.noreply.github.com>
2019-09-15 23:22:46 +0200

    Update xpath.py
    Each time this function is used a new seed will be generated

--- instapy/xpath.py
+++ instapy/xpath.py
@@ -1,5 +1,10 @@
 from .xpath_compile import xpath
+from .util import new_seed
+
 
 
 def read_xpath(function_name, xpath_name):
+    # new seed each time this function is called
+    new_seed()
+    
     return xpath[function_name][xpath_name]
