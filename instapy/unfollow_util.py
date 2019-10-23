ca8c5c471838b2206dfc4e7ac40525c2a2e45d21
Şəhriyar Cəfər Qulu <shahriyarguliyev@hotmail.com>
2019-10-24 02:22:49 +0400

    Fix: make it follow properly by adding little required delay
    Without that delay added, it goes to refresh page and try to click follow button again and wait even more - 4+4 seconds - saying "It 'verified' follow after reloading page"..

--- instapy/unfollow_util.py
+++ instapy/unfollow_util.py
@@ -607,6 +607,8 @@ def follow_user(browser, track, login, user_name, button, blacklist, logger, log
         )
         if following_status in ["Follow", "Follow Back"]:
             click_visibly(browser, follow_button)  # click to follow
+            sleep(3)  # wait for action complete
+
             follow_state, msg = verify_action(
                 browser, "follow", track, login, user_name, None, logger, logfolder
             )
