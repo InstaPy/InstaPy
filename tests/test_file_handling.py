579f1ce90042f969b79c9debbb275509fdf3b669
IreneArapogiorgi <irenearapogiorgi@gmail.com>
2020-04-29 21:49:40 +0300

    Fix test files' path

--- tests/test_file_handling.py
+++ tests/test_file_handling.py
@@ -12,19 +12,15 @@ class TestFileHandling(unittest.TestCase):
 
     def test_valid_entries(self):
         """Check file_handling with valid entries"""
-        actual_entries = util.file_handling("tests/resources/valid_target_list.txt")
+        actual_entries = util.file_handling("test/resources/valid_target_list.txt")
         self.assertListEqual(actual_entries, expected_entries)
 
     def test_invalid_entries(self):
         """Check file_handling with invalid entries"""
-        actual_entries = util.file_handling("tests/resources/invalid_target_list.txt")
+        actual_entries = util.file_handling("test/resources/invalid_target_list.txt")
         self.assertListEqual(actual_entries, expected_entries)
 
     def test_invalid_filepath(self):
         """Check whether it raises an exception when the filepath is invalid"""
         actual_entries = util.file_handling("invalid_filepath")
         self.assertListEqual(actual_entries, ["FileNotFoundError"])
-
-
-if __name__ == "__main__":
-    unittest.main()
