573f483fdff71247958e07fedcdc90968bea9220
Felix Breuer <f.breuer94@gmail.com>
2020-11-13 15:05:38 +0100

    Feature/docusaurus (#5888)
    Convert Documentation to Docusaurus
    
    Signed-off-by: Felix Breuer <f.breuer94@gmail.com>
    
    Co-authored-by: Tim Großmann <contact.timgrossmann@gmail.com>

---
+++ docusaurus/sidebars.js
@@ -0,0 +1,14 @@
+module.exports = {
+  docs: {
+    Documentation: [
+      'home',
+      'settings',
+      'actions',
+      'third-party-features',
+      'instance-settings',
+      'relationship-tools',
+      'automate-instapy',
+      'additional-information',
+    ],
+  },
+};
