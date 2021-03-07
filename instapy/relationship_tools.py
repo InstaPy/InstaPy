62a005acc551ce14d0f23b90e415813ef7817162
Paul Schroeder <pschroeder@splunk.com>
2021-03-06 18:08:35 -0700

    add flag for verified_only

--- instapy/relationship_tools.py
+++ instapy/relationship_tools.py
@@ -30,6 +30,7 @@ def get_followers(
     store_locally,
     logger,
     logfolder,
+    verified_only=False
 ):
     """ Get entire list of followers using graphql queries. """
 
@@ -37,6 +38,7 @@ def get_followers(
     user_data = {}
     variables = {}
     all_followers = []
+    verified_followers = []
     sc_rolled = 0
     grab_notifier = False
     local_read_failure = False
@@ -184,7 +186,12 @@ def get_followers(
             # get followers
             page_info = data["user"]["edge_followed_by"]["page_info"]
             edges = data["user"]["edge_followed_by"]["edges"]
+
             for user in edges:
+                if verified_only:
+                    if user["node"]["is_verified"]:
+                        verified_followers.append(user["node"]["username"])
+
                 all_followers.append(user["node"]["username"])
 
             grabbed = len(set(all_followers))
@@ -307,8 +314,10 @@ def get_followers(
     sleep(sleep_t)
     logger.info("Yawn :] let's go!\n")
 
-    return all_followers
-
+    if verified_only:
+        return verified_followers
+    else:
+        return all_followers
 
 def get_following(
     browser,
