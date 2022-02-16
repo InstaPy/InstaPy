3c81e5d7cdcb1dad0ec89882cb12b409c96ba637
ravicii <56163105+mr-ravicii@users.noreply.github.com>
2022-02-16 17:56:32 +0530

    Update xpath_compile.py file
    Instagram made significant changes to their XPATH structure a couple of weeks ago. This pull request summarises the fixes I've identified which works for me.

--- instapy/xpath_compile.py
+++ instapy/xpath_compile.py
@@ -76,8 +76,8 @@ xpath["get_active_users"] = {
 }
 
 xpath["get_buttons_from_dialog"] = {
-    "follow_button": "//button[text()='Follow']",
-    "unfollow_button": "//button[text() = 'Following']",
+    "follow_button": "//button/div[text()='Follow']",
+    "unfollow_button": "//button/div[text() = 'Following']",
 }
 
 xpath["get_comment_input"] = {
@@ -95,12 +95,12 @@ xpath["get_comments_on_post"] = {
 xpath["get_cord_location"] = {"json_text": "//body"}
 
 xpath["get_following_status"] = {
-    "follow_button_XP": "//button[text()='Following' or \
-                                  text()='Requested' or \
-                                  text()='Follow' or \
-                                  text()='Follow Back' or \
-                                  text()='Unblock' and not(ancestor::*/@role = 'presentation')]",
-    "follow_span_XP_following": "//button/div/span[contains(@aria-label, 'Following')]",
+    "follow_button_XP": "//button/div[text()='Following' or \
+                                      text()='Requested' or \
+                                      text()='Follow' or \
+                                      text()='Follow Back' or \
+                                      text()='Unblock' and not(ancestor::*/@role = 'presentation')]",
+    "follow_span_XP_following": "//button/div/div/span[@aria-label='Following']",
 }
 
 xpath["get_follow_requests"] = {
@@ -108,7 +108,7 @@ xpath["get_follow_requests"] = {
     "view_more_button": "//button[text()='View More']",
 }
 
-xpath["get_given_user_followers"] = {"followers_link": "//ul/li[2]/a/span"}
+xpath["get_given_user_followers"] = {"followers_link": "//ul/li[2]/a/div/span"}
 
 xpath["get_given_user_following"] = {
     "all_following": "//a[contains(@href,'following')]/span",
@@ -116,7 +116,7 @@ xpath["get_given_user_following"] = {
     "following_link": '//a[@href="/{}/following/"]',
 }
 
-xpath["get_photo_urls_from_profile"] = {"photos_a_elems": "//div/a"}
+xpath["get_photo_urls_from_profile"] = {"photos_a_elems": "//a[starts-with(@href,'/p')]/@href"}
 
 xpath["get_links_for_location"] = {
     "top_elements": "//main/article/div[1]",
@@ -150,7 +150,7 @@ xpath["get_source_link"] = {
 
 xpath["get_users_through_dialog"] = {"find_dialog_box": "//body/div[4]/div/div[2]"}
 
-xpath["is_private_profile"] = {"is_private": '//h2[@class="_kcrwx"]'}
+xpath["is_private_profile"] = {"is_private": '//h2[@class="rkEop"]'}
 
 xpath["like_comment"] = {
     "comments_block": "//*[contains(@class,'EtaWk')]",
@@ -191,7 +191,7 @@ xpath["open_comment_section"] = {
 }
 
 xpath["unfollow"] = {
-    "following_link": "//ul/li[3]/a/span",
+    "following_link": "//ul/li[3]/a/div/span",
     "find_dialog_box": "//section/main/div[2]",
 }
 
